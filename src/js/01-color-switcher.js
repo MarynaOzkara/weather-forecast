903871e7200c7c039405ed32e6f6dc75c39d6f18
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-16 23:01:46 +0300

    fix task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,18 +1,22 @@
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 
 startBtn.addEventListener('click', () => {
+    
     timerId = setInterval(() => {
         let hexColor = getRandomHexColor();
         function getRandomHexColor() {
             return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
           }
           body.style.backgroundColor = hexColor;
-    }, 1000);
+    }, 1000); 
+    startBtn.setAttribute('disabled', true);
+ 
 })
 stopBtn.addEventListener('click', () => {
     clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
 });
 
 
