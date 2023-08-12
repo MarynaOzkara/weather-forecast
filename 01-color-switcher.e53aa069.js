6d3abe79c06239537c85cb5e2dc8d3d4cd4d20a5
MarynaOzkara <126406516+MarynaOzkara@users.noreply.github.com>
2023-08-12 20:48:17 +0000

    Deploying to gh-pages from @ MarynaOzkara/weather-forecast@eef3b4504504d73a03dca060b60b8afb880f371d 🚀

--- 01-color-switcher.e53aa069.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.e53aa069.js.map
